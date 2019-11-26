068550454635243db2f8cbc4e1ec71588ca83cc1
shuimuyangsha <1023178387@qq.com>
2019-11-26 12:10:59 +0800

    创建第五章工程 操作符和表达式

---
+++ Chapter5_OperatorsAndExpressions/main.c
@@ -0,0 +1,36 @@
+
+///*第四题*/
+#include "stdio.h"
+#include "math.h"
+#include "string.h"
+#include "bit_array.h"
+char bit_array[100];
+unsigned int bit_number;
+int value;
+int result;
+int main(void)
+{
+	value = 1;
+	bit_number = 4;
+	memset(bit_array,1,sizeof(bit_array));
+
+	//set_bit(bit_array, bit_number);
+	//clear_bit(bit_array, bit_number);
+	//for (int i = 0; i < 24;i++) {
+	//	set_bit(bit_array, i);
+	//}
+
+	//for (int i = 26; i < 40; i++) {
+	//	clear_bit(bit_array, i);
+	//}
+	//assign_bit(bit_array, bit_number, value);
+	for (int j = 0; j < 50; j++) {
+		result = test_bit(bit_array, j);
+		printf("第 %3d 位的值为%d\r\n", j, result);
+	}
+
+
+	while (1);
+	return 0;
+}
+
--- Chapter6_Pointers/main.c
+++ Chapter6_Pointers/main.c
@@ -54,7 +54,32 @@
 //}
 
 
-/*第四题*/
+///*第四题*/
+//#include "math.h"
+//#include "stdio.h"
+//#include "string.h"
+//#include "eratosthenes.h"
+//#include "time.h"
+//
+//clock_t SpendTime;
+//
+//#define AMOUNT  (int)100000		//总数
+//char num[AMOUNT] = { TRUE };	//记录是否为质数
+//int PrimeNumber = 0;	//质数的个数
+//int main(void) {
+//	memset(&num, TRUE, AMOUNT);
+//
+//	PrimeNumber = eratosthenes(num,AMOUNT);
+//	printf("质数有%d个。\r\n", PrimeNumber);
+//	SpendTime = clock();
+//	printf("程序开始执行后占用处理器时间%d\r\n", SpendTime);
+//	while (1);
+//	return 0;
+//}
+
+
+
+/*第五题*/
 #include "math.h"
 #include "stdio.h"
 #include "string.h"
@@ -69,11 +94,10 @@ int PrimeNumber = 0;	//质数的个数
 int main(void) {
 	memset(&num, TRUE, AMOUNT);
 
-	PrimeNumber = eratosthenes(num,AMOUNT);
+	PrimeNumber = eratosthenes(num, AMOUNT);
 	printf("质数有%d个。\r\n", PrimeNumber);
 	SpendTime = clock();
 	printf("程序开始执行后占用处理器时间%d\r\n", SpendTime);
 	while (1);
 	return 0;
 }
-
